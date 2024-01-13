9e5e1d37e5b038a336495871cf4f23e0fa980080
Yaroslav Kozakov <ykzlearn@gmail.com>
2024-01-13 21:28:57 +0200

    Added conftest.py

---
+++ conftest.py
@@ -0,0 +1,34 @@
+import pytest
+
+
+class User:
+
+    def __init__(self) -> None:
+        self.name = None
+        self.second_name = None
+
+    def create(self):
+        self.name = "Sergii"
+        self.second_name = "Butenko"
+
+    def remove(self):
+        self.name = ""
+        self.second_name = ""
+
+
+@pytest.fixture
+def user():
+    user = User()
+    user.create()
+
+    yield user
+
+    user.remove()
+
+
+def test_change_name(user):
+    assert user.name == 'Sergii'
+
+
+def test_change_second_name(user):
+    assert user.second_name == 'Butenko'
