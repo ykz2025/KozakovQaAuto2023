9f7533d8e1a2fa6388c10ff6c11654a2d9b6d14a
Yaroslav Kozakov <ykzlearn@gmail.com>
2024-02-01 21:35:55 +0200

    modified pytest.ini test_ui.py new base_page.py sign_in_page.py

---
+++ tests/ui/test_ui.py
@@ -0,0 +1,38 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+@pytest.mark.ui
+def test_check_incorrect_username():
+    # Створення об'єкту для керування браузером
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    # відкриваємо сторінку https://github.com/login
+    driver.get('https://github.com/login')
+
+    # Знаходимо поле, в яке будемо вводити неправильне ім'я користувача або поштову адресу
+    login_elem = driver.find_element(By.ID, "login_field")
+
+    # Вводимо неправильне ім'я користувача або поштову адресу
+    login_elem.send_keys("sergiibutenko@mistakeinemail.com")
+
+    # Знаходимо поле, в яке будемо вводити неправильний пароль
+    pass_elem = driver.find_element(By.ID, "password")
+
+    # Вводимо неправильний пароль
+    pass_elem.send_keys("wrong password")
+
+    # Знаходимо кнопку sign in
+    btn_elem = driver.find_element(By.NAME, "commit")
+
+    # Емулюємо клік лівою кнопкою мишки
+    btn_elem.click()
+
+    # Перевіряємо, що назва сторінки така, яку ми очікуємо
+    assert driver.title == "Sign in to GitHub - GitHub"
+
+    # Закриваємо браузер
+    driver.close()
